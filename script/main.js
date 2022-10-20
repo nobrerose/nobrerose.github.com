46745eeb452cd17f20d7121cf3fa65d1f2f12a39
Daniel Pereira <danielpereirasuporte@gmail.com>
2022-10-19 21:37:52 -0300

    Create main.js

---
+++ script/main.js
@@ -0,0 +1,22 @@
+const app = new Vue({
+	el: "#root",
+	data: {
+		ui: {
+			header: "Cantinho nobre rose",
+			description: "Sua beleza é nosso dever e responsabilidade",
+			contact_link: "https://bit.ly/cantinhonobrerose"
+		},
+		product: {
+			cosmeticos: {
+				prefix: "./img/cosmeticos/cp02-cosmeticos-v2-00",
+				min: 1,
+				max: 184,
+			},
+		}
+	},
+	methods: {
+		getCatalog: function() {
+			window.location.href="#catalog";
+		}
+	}
+});
